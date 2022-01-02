c5d2b068a496dfe6b4ad8e86e5ec7fd3cf6af30f
Thinh Nguyen <thanhthinhcntt@gmail.com>
2022-01-02 13:19:05 +0700

    add

--- news/news/App_Start/BundleConfig.cs
+++ news/news/App_Start/BundleConfig.cs
@@ -8,23 +8,23 @@ namespace news
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include("~/Scripts/jquery.min.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
-
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
-
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+            bundles.Add(new ScriptBundle("~/bundles/scripts").Include(
+                "~/Scripts/jquery-ui.min.js",
+                "~/Scripts/bootstrap.bundle.min.js",
+                "~/Scripts/metismenu.min.js",
+                "~/Scripts/waves.js",
+                "~/Scripts/feather.min.js",
+                "~/Scripts/jquery.slimscroll.min.js",
+                "~/plugins/apexcharts/apexcharts.min.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                "~/Content/bootstrap.min.css",
+                "~/Content/jquery-ui.min.css",
+                "~/Content/icons.min.css",
+                "~/Content/metisMenu.min.css",
+                "~/Content/app.min.css"));
         }
     }
 }
